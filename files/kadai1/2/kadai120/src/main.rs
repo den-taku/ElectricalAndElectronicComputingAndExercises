b33ff9ab64ec08cf42755c29c223fedfc4dd88f2
DenTaku <dentaku1412@gmail.com>
2020-10-01 14:49:53 +0900

    Define Matrix<T>

--- files/kadai1/2/kadai120/src/main.rs
+++ files/kadai1/2/kadai120/src/main.rs
@@ -1,11 +1,26 @@
-// enum Matrix { Vec<Vec<f64>>, }
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct Matrix<T> 
+{
+    n: i64,
+    m: i64,
+    array: Vec<Vec<T>>,
+}
 
+impl<T> Matrix<T> {
+    //
+}
 
 fn calculate_y(n: usize, A: &Vec<f64>) -> Vec<f64> {
     vec![0.0]
 }
 
-
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "{:?}",
+        Matrix {
+            array: vec![vec![0.0]],
+            n: 1,
+            m: 1
+        }
+    );
 }
