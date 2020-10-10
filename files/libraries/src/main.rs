1928b7230ce02353c850fcf020c0ba85906552c9
DenTaku <dentaku1412@gmail.com>
2020-10-10 19:56:20 +0900

    Make newton_raphson_method use Rc<T>

--- files/libraries/src/main.rs
+++ files/libraries/src/main.rs
@@ -2,16 +2,31 @@ mod bisection_method;
 mod matrix;
 mod newton_raphson_method;
 
+use std::rc::Rc;
+
 fn main() {
-    let f = Box::new(|x: f64| -> f64 {
+    let f: Rc<Box<dyn Fn(f64) -> f64>> = Rc::new(Box::new(|x: f64| -> f64 {
         x.powf(5.) - 3. * x.powf(4.) + x.powf(3.) + 5. * x.powf(2.) - 6. * x + 2.
-    });
+    }));
     println!(
         "{}",
-        if let Ok(v) = newton_raphson_method::newton_raphson_method::newton_raphson_method(f, -1.) {
+        if let Ok(v) =
+            newton_raphson_method::newton_raphson_method::newton_raphson_method(f.clone(), -1.)
+        {
             v
         } else {
             0.0
         }
     );
+    // This call induce stack overflow
+    // println!(
+    //     "{}",
+    //     if let Ok(v) =
+    //         newton_raphson_method::newton_raphson_method::newton_raphson_method(f.clone(), 0.6)
+    //     {
+    //         v
+    //     } else {
+    //         0.0
+    //     }
+    // );
 }
--- files/libraries/src/newton_raphson_method.rs
+++ files/libraries/src/newton_raphson_method.rs
@@ -2,9 +2,10 @@ pub mod newton_raphson_method {
     pub use std::rc::Rc;
     pub use std::result::Result;
 
-    pub fn newton_raphson_method(f: Box<dyn Fn(f64) -> f64>, init: f64) -> Result<f64, f64> {
+    // f must be declared as dyn Fn trait object.
+    pub fn newton_raphson_method(f: Rc<Box<dyn Fn(f64) -> f64>>, init: f64) -> Result<f64, f64> {
         let threshold = 0.1e-10;
-        let f = Rc::new(f);
+        // let f = Rc::new(f);
         let f_dir = differential_f(f.clone()); // f is consumed here.
         if !f.is_converge() {
             return Err(f64::NEG_INFINITY);
@@ -21,7 +22,7 @@ pub mod newton_raphson_method {
 
     fn newton_transform(
         f: Rc<Box<dyn Fn(f64) -> f64>>,
-        f_dir: Rc<dyn Fn(f64) -> f64>,
+        f_dir: Rc<Box<dyn Fn(f64) -> f64>>,
     ) -> Rc<Box<dyn Fn(f64) -> f64>> {
         Rc::new(Box::new(move |x: f64| -> f64 { x - f(x) / f_dir(x) }))
     }
@@ -66,9 +67,9 @@ mod tests_newton_raphson_method {
 
     #[test]
     fn test_newton_raphson_method_newton_raphson_method() {
-        let f = Box::new(|x: f64| -> f64 {
+        let f: Rc<Box<dyn Fn(f64) -> f64>> = Rc::new(Box::new(|x: f64| -> f64 {
             x.powf(5.) - 3. * x.powf(4.) + x.powf(3.) + 5. * x.powf(2.) - 6. * x + 2.
-        });
+        }));
         assert_eq!(newton_raphson_method(f, -1.), Ok(-1.4142135623730951));
     }
 }
