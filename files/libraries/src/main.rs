2baa218f0ac7fa08d7c1da26ccc3840780585468
DenTaku <dentaku1412@gmail.com>
2020-10-11 13:32:13 +0900

    Plot f(x)

--- files/libraries/src/main.rs
+++ files/libraries/src/main.rs
@@ -8,14 +8,14 @@ fn main() {
     let f: Rc<dyn Fn(f64) -> f64> = Rc::new(|x: f64| -> f64 {
         x.powf(5.) - 3. * x.powf(4.) + x.powf(3.) + 5. * x.powf(2.) - 6. * x + 2.
     });
-    println!(
-        "{}",
-        if let Ok(v) = newton_raphson_method::newton_raphson_method(f.clone(), -1.) {
-            v
-        } else {
-            0.0
-        }
-    );
+    // println!(
+    //     "{}",
+    //     if let Ok(v) = newton_raphson_method::newton_raphson_method(f.clone(), -1.) {
+    //         v
+    //     } else {
+    //         0.0
+    //     }
+    // );
     // This call induce stack overflow
     // println!(
     //     "{}",
@@ -27,4 +27,7 @@ fn main() {
     //         0.0
     //     }
     // );
+    for i in (9972)..(10016) {
+        println!("{}, {},", (i as f64) / 10000., f((i as f64) / 10000.));
+    }
 }
