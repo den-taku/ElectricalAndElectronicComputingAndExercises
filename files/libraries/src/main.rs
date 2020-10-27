0925dc2556f462cacad695378e2ff75f256f70fe
DenTaku <dentaku1412@gmail.com>
2020-10-28 03:33:37 +0900

    Done kadai1.3.1

--- files/libraries/src/main.rs
+++ files/libraries/src/main.rs
@@ -19,29 +19,42 @@ fn main() {
     vec_f.push(f1.clone());
     vec_f.push(f2.clone());
 
-    let data = newton_raphson_method::jacobian_newton_raphson_method(
-        vec_f,
+    let _data = newton_raphson_method::jacobian_newton_raphson_method(
+        vec_f.clone(),
         vec![2.0f64.sqrt(); 2],
         vec![0f64, 0f64],
     )
     .unwrap();
 
+    let data2 = newton_raphson_method::jacobian_newton_raphson_method(
+        vec_f, 
+        vec![2f64.sqrt(); 2],
+        vec![1f64;2]
+    ).unwrap();
+
+
+    let x: Vec<f64> = data2.iter().map(|e| e.0).collect();
+    let y: Vec<f64> = data2.iter().map(|e| (e.1[0].powf(2.0) + e.1[1].powf(2.0)).sqrt()).collect();
+
+    println!("{:?}", x);
+    println!("{:?}", y);
+
     let mut fg = Figure::new();
     {
-        let axes = fg.axes2d();
-        // x軸を表示
-        axes.set_x_axis(true, &[]);
-        // 表示範囲の指定
-        axes.set_x_range(Fix(0.9), Fix(1.2));
-        axes.set_y_range(Fix(0.9), Fix(1.2));
-
-        data.iter().fold((), |_, e| {
-            axes.points(
-                &[e.1[0].clone()],
-                &[e.1[1].clone()],
-                &[Color("blue"), PointSymbol('O')],
-            );
-        });
+        let _axec = fg
+        .axes2d()
+        .set_x_axis(true, &[])
+        .set_x_range(Fix(0.0), Fix(5.0))
+        .set_y_range(Fix(1.0e-12), Fix(2.0))
+        .set_y_log(Some(10.0))
+        .set_x_label("times", &[])
+        .set_y_label("error", &[])
+        // .set_y_ticks(Some((Fix(-12.0), 1)), &[], &[])
+        .lines(
+            x,
+            y,
+            &[Caption("bisection_method"), Color("blue")],
+        );
     }
     let _ = fg.show();
 }
