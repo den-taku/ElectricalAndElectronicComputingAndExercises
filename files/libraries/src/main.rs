43c5171abf2e5cd0ba2638c01ed499c6597f7401
DenTaku <dentaku1412@gmail.com>
2021-01-14 10:56:14 +0900

    Get parameter

--- files/libraries/src/main.rs
+++ files/libraries/src/main.rs
@@ -255,80 +255,80 @@ fn main() {
     // let matrixa = data::matrix21a_f64();
     // let matrixb = data::matrix21b_f64();
 
-    let mut data = Vec::new();
-    for i in 1..2000 {
-       let mut sor = SOR::<f64>::new(matrixa.clone(), matrixb.clone(), Matrix::new(9, 1), i as f64 / 1000.0);
-       let omega = i as f64 / 1000.0;
-       let times = sor.solve(10e-10, 10_000).pop().unwrap().0;
-       data.push((omega, times));
-       println!("ω: {}, times: {}", omega, times);
-    }
-    let mut fg = Figure::new();
-    {
-        let axec = fg
-            .axes2d()
-            .set_x_axis(true, &[])
-            .set_x_range(Fix(0.0), Fix(2.0))
-            .set_y_range(Fix(0.0), Fix(10000.0))
-            // .set_y_log(Some(10.0))
-            .set_x_label("ω", &[])
-            .set_y_label("times", &[]);
-            // .set_y_ticks(Some((Fix(-12.0), 1)), &[], &[])
-            // .lines(x, y, &[Caption("newton"), Color("blue")]);
+    // let mut data = Vec::new();
+    // for i in 1..2000 {
+    //    let mut sor = SOR::<f64>::new(matrixa.clone(), matrixb.clone(), Matrix::new(9, 1), i as f64 / 1000.0);
+    //    let omega = i as f64 / 1000.0;
+    //    let times = sor.solve(10e-10, 10_000).pop().unwrap().0;
+    //    data.push((omega, times));
+    //    println!("ω: {}, times: {}", omega, times);
+    // }
+    // let mut fg = Figure::new();
+    // {
+    //     let axec = fg
+    //         .axes2d()
+    //         .set_x_axis(true, &[])
+    //         .set_x_range(Fix(0.0), Fix(2.0))
+    //         .set_y_range(Fix(0.0), Fix(10000.0))
+    //         // .set_y_log(Some(10.0))
+    //         .set_x_label("ω", &[])
+    //         .set_y_label("times", &[]);
+    //         // .set_y_ticks(Some((Fix(-12.0), 1)), &[], &[])
+    //         // .lines(x, y, &[Caption("newton"), Color("blue")]);
 
-        data.iter().fold((), |_, e| {
-            axec.points(&[e.0], &[e.1], &[Color("blue"), PointSymbol('O')]);
-        })
-    }
-    let _ = fg.show();
+    //     data.iter().fold((), |_, e| {
+    //         axec.points(&[e.0], &[e.1], &[Color("blue"), PointSymbol('O')]);
+    //     })
+    // }
+    // let _ = fg.show();
 
     // let matrixa = data::matrix23a_f64();
     // let matrixb = data::matrix23b_f64();
     // println!("{}", Matrix::solve_eqn_gauss(&matrixa, &matrixb));
     // println!("{}", Matrix::solve_eqn(&matrixa, &matrixb));
 
-    // let mut jacobi = Jacobi::<f64>::new(matrixa.clone(), matrixb.clone(), Matrix::new(9, 1));
-    // let mut data_jacobi = jacobi.solve(10e-10, 10_000);
-    // println!("{}", jacobi);
-    // println!("{:?}", data_jacobi.pop().unwrap());
+    let mut jacobi = Jacobi::<f64>::new(matrixa.clone(), matrixb.clone(), Matrix::new(9, 1));
+    let mut data_jacobi = jacobi.solve(10e-10, 10_000);
+    println!("{}", jacobi);
+    println!("{:?}", data_jacobi.pop().unwrap());
 
-    // let mut gauss_seidel =
-    //     GaussSeidel::<f64>::new(matrixa.clone(), matrixb.clone(), Matrix::new(9, 1));
-    // let mut data_gauss_seidel = gauss_seidel.solve(10e-10, 10_000);
-    // println!("{}", gauss_seidel);
-    // println!("{:?}", data_gauss_seidel.pop().unwrap());
+    let mut gauss_seidel =
+        GaussSeidel::<f64>::new(matrixa.clone(), matrixb.clone(), Matrix::new(9, 1));
+    let mut data_gauss_seidel = gauss_seidel.solve(10e-10, 10_000);
+    println!("{}", gauss_seidel);
+    println!("{:?}", data_gauss_seidel.pop().unwrap());
 
-    // let mut sor = SOR::<f64>::new(matrixa.clone(), matrixb.clone(), Matrix::new(9, 1), 1.8);
-    // let mut data_sor = sor.solve(10e-10, 10_000);
-    // println!("{}", sor);
-    // println!("{:?}", data_sor.pop().unwrap());
+    let mut sor = SOR::<f64>::new(matrixa.clone(), matrixb.clone(), Matrix::new(9, 1), 1.852);
+    let mut data_sor = sor.solve(10e-10, 10_000);
+    println!("{}", sor);
+    println!("{:?}", data_sor.pop().unwrap());
 
-    //  let mut fg = Figure::new();
+     let mut fg = Figure::new();
 
-    //  let x_jacobi: Vec<f64> = data_jacobi.iter().map(|e| e.0).collect();
-    //  let y_jacobi: Vec<f64> = data_jacobi.iter().map(|e| e.1).collect();
-    //  let x_gause_seidel: Vec<f64> = data_gauss_seidel.iter().map(|e| e.0).collect();
-    //  let y_gause_seidel: Vec<f64> = data_gauss_seidel.iter().map(|e| e.1).collect();
-    //  let x_sor: Vec<f64> = data_sor.iter().map(|e| e.0).collect();
-    //  let y_sor: Vec<f64> = data_sor.iter().map(|e| e.1).collect();
-    //  {
-    //      let _axec = fg
-    //          .axes2d()
-    //          .set_x_axis(true, &[])
-    //          .set_x_range(Fix(0.0), Fix(10000.0))
-    //          .set_y_range(Fix(20e-6), Fix(4000000.0))
-    //          .set_y_log(Some(10.0))
-    //          .set_x_label("times", &[])
-    //          .set_y_label("residual\\_norm", &[])
-    //          .lines(x_jacobi, y_jacobi, &[Caption("Jacobi"), Color("green")])
-    //          .lines(
-    //              x_gause_seidel,
-    //              y_gause_seidel,
-    //              &[Caption("GaussSeidel"), Color("red")],
-    //          )
-    //          .lines(x_sor, y_sor, &[Caption("SOR"), Color("blue")]);
-    //  }
-    //  let _ = fg.show();
+     let x_jacobi: Vec<f64> = data_jacobi.iter().map(|e| e.0).collect();
+     let y_jacobi: Vec<f64> = data_jacobi.iter().map(|e| e.1).collect();
+     let x_gause_seidel: Vec<f64> = data_gauss_seidel.iter().map(|e| e.0).collect();
+     let y_gause_seidel: Vec<f64> = data_gauss_seidel.iter().map(|e| e.1).collect();
+     let x_sor: Vec<f64> = data_sor.iter().map(|e| e.0).collect();
+     let y_sor: Vec<f64> = data_sor.iter().map(|e| e.1).collect();
+     {
+         let _axec = fg
+             .axes2d()
+             .set_x_axis(true, &[])
+             .set_x_range(Fix(0.0), Fix(10000.0))
+             .set_y_range(Fix(20e-6), Fix(4000000.0))
+             .set_y_log(Some(10.0))
+             .set_x_label("times", &[])
+             .set_y_label("residual\\_norm", &[])
+             .lines(x_jacobi, y_jacobi, &[Caption("Jacobi"), Color("green")])
+             .lines(
+                 x_gause_seidel,
+                 y_gause_seidel,
+                 &[Caption("GaussSeidel"), Color("red")],
+             )
+             .lines(x_sor, y_sor, &[Caption("SOR"), Color("blue")]);
+     }
+     let _ = fg.show();
 
     // println!(
     //     "{}",
