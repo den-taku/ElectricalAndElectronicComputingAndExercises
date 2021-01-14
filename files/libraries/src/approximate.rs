9e65cf4c60d95f5f0af70e00404206aa86eaaa5f
DenTaku <dentaku1412@gmail.com>
2021-01-14 20:24:20 +0900

    Reimplement euler2

--- files/libraries/src/approximate.rs
+++ files/libraries/src/approximate.rs
@@ -27,6 +27,7 @@ where
             * (v_y_n_1 + F::from_f64(-f64::cos(now)).unwrap()))
     .sqrt();
     log.1.push((t, err_norm));
+    println!("      {}", err_norm.to_f64().unwrap());
 
     let now = F::from_f64(now).unwrap();
 
@@ -38,29 +39,69 @@ where
     }
 }
 
-/// v_x_n+1 = v_x + h * v_y
-/// v_y_n+1 = v_y - h * v_x
-/// v_z_n+1 = 0
 pub fn euler2<F>(
+    v_x: F,
+    v_y: F,
     h: F,
-    now: F,
-    log: (&mut Vec<(F, F)>, &mut Vec<(F, F)>), // (data, norm)
-) where
+    t: F,
+    max: F, 
+) -> F
+where
     F: Float + FromPrimitive,
 {
-    let (v_x, v_y) = log.0.pop().unwrap();
-    log.0.push((v_x, v_y));
     let v_x_n_1 = v_x + h * v_y;
     let v_y_n_1 = v_y - h * v_x;
-    log.0.push((v_x_n_1, v_y_n_1));
+    // log.0.push((v_x_n_1, v_y_n_1));
 
-    let now = now.to_f64().unwrap();
+    let now = (t + h).to_f64().unwrap();
 
     let err_norm = ((v_x_n_1 + F::from_f64(-f64::sin(now)).unwrap())
         * (v_x_n_1 + F::from_f64(-f64::sin(now)).unwrap())
         + (v_y_n_1 + F::from_f64(-f64::cos(now)).unwrap())
             * (v_y_n_1 + F::from_f64(-f64::cos(now)).unwrap()))
     .sqrt();
-    let log_err_norm = F::log2(err_norm);
-    log.1.push((F::from_f64(now).unwrap(), log_err_norm));
+    // log.1.push((t, err_norm));
+
+    let now = F::from_f64(now).unwrap();
+
+    let max = if max >= err_norm {
+        max
+    } else {
+        err_norm
+    };
+
+    // while 0 <= t <= 5π
+    if now <= F::from_f64(5.0).unwrap() * F::from_f64(PI).unwrap() {
+        euler2(v_x_n_1, v_y_n_1, h, now, max)
+    } else {
+        max
+    }
 }
+
+// v_x_n+1 = v_x + h * v_y
+// v_y_n+1 = v_y - h * v_x
+// v_z_n+1 = 0
+// pub fn euler2_dash<F>(
+//     h: F,
+//     now: F,
+//     log: (&mut Vec<(F, F)>, &mut Vec<(F, F)>), // (data, norm)
+// ) where
+//     F: Float + FromPrimitive,
+// {
+//     let (v_x, v_y) = log.0.pop().unwrap();
+//     log.0.push((v_x, v_y));
+//     let v_x_n_1 = v_x + h * v_y;
+//     let v_y_n_1 = v_y - h * v_x;
+//     log.0.push((v_x_n_1, v_y_n_1));
+
+//     let now = now.to_f64().unwrap();
+
+//     let err_norm = ((v_x_n_1 + F::from_f64(-f64::sin(now)).unwrap())
+//         * (v_x_n_1 + F::from_f64(-f64::sin(now)).unwrap())
+//         + (v_y_n_1 + F::from_f64(-f64::cos(now)).unwrap())
+//             * (v_y_n_1 + F::from_f64(-f64::cos(now)).unwrap()))
+//     .sqrt();
+//     let log_err_norm = F::log2(err_norm);
+//     // println!("       {}", log_err_norm.to_f64().unwrap());
+//     log.1.push((F::from_f64(now).unwrap(), log_err_norm));
+// }
--- files/libraries/src/main.rs
+++ files/libraries/src/main.rs
@@ -24,26 +24,31 @@ fn main() {
         true_y.push(-f64::cos(2.0 * PI * i as f64 / 100.0));
     }
 
-    let mut data: Vec<(f64, f64)> = Vec::new();
-    data.push((0.0, -1.0));
-    let mut err: Vec<(f64, f64)> = Vec::new();
+    
+
+    let mut errs = Vec::new();
 
     let mut now = 0.0;
     let tau = 2.0 * PI;
     for i in 3..19 {
+        println!("p = {}", i);
         let h = tau * 2f64.powf(- i as f64);
-        now += h;
-        // println!("now: {}", now);
-        euler2::<f64>(h, now, (&mut data, &mut err));
+        let max = euler2::<f64>(0.0, -1.0, h, 0.0, 0.0);
+        // err.sort_by(|a, b| a.partial_cmp(&b).unwrap());
+        errs.push((i as f64, max));
     }
-    let log = (data, err);
-    log.1.iter().fold((), |_, e| {
-        println!("when {}, {}", e.0, e.1);
-    });
+    let errs: Vec<(f64, f64)> = errs.iter().map(|e| {
+        (e.0, f64::log2(e.1))
+    }).collect();
+    // let log = (data, err);
+    // log.1.iter().fold((), |_, e| {
+    //     println!("when {}, {}", e.0, e.1);
+    // });
 
     // let log = euler::<f64>(0.0, -1.0, PI / 32.0, 0.0, (data, err));
 
-    draw_graph(0.0, PI, 0.0, 1.6, "x", "log_2E_r", "blue", log.1.clone());
+    draw_graph(2.9, 18.1, 0.0, 1.6, "x", "log_2E_r", "blue", errs);
+    // draw_graph(0.0, 1.7, 0.0, 1.6, "x", "log_2E_r", "blue", log.1.clone());
     // draw_graph(-4.0, 4.0, -4.0, 4.0, "x", "y", "blue", log.0.clone());
     // println!("{:?}", &log.1);
     // let mut fg = Figure::new();
