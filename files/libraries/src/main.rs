de25ec8d72ddfd0294f52e75f50a9cf6c453fe51
DenTaku <dentaku1412@gmail.com>
2021-01-15 20:30:33 +0900

    Implement runge4

--- files/libraries/src/main.rs
+++ files/libraries/src/main.rs
@@ -20,46 +20,32 @@ use runge_kutta4::*;
 use std::f64::consts::PI;
 
 fn main() {
-    let mut errs = Vec::new();
-
-    let tau = 2.0 * PI;
-    for i in 3..19 {
-        println!("p = {}", i);
-        let h = tau * 2f64.powf(-i as f64);
-        let max = heun2::<f64>(0.0, -1.0, h, 0.0, 0.0);
-        // err.sort_by(|a, b| a.partial_cmp(&b).unwrap());
-        errs.push((i as f64, max));
-    }
-    println!("{:?}", &errs);
-    let errs: Vec<(f64, f64)> = errs.iter().map(|e| (e.0, f64::log2(e.1))).collect();
-
-    println!("{:?}", &errs);
-
-    let (a, b) = least_squares_method(errs.clone());
-    println!("The func is {} p + {}.", a, b);
-    // draw_graph(2.9, 18.1, 0.0, 7.0, "p", "log_2E_r", "blue", errs);
-    let mut line_x = Vec::new();
-    let mut line_y = Vec::new();
-    for i in 0..20 {
-        line_x.push(i as f64);
-        line_y.push(-0.13478047683713912 * i as f64 + 2.8352123147754655);
+    let mut true_x = vec![];
+    let mut true_y = vec![];
+    for i in 0..101 {
+        true_x.push(-f64::sin(2.0 * PI * i as f64 / 100.0));
+        true_y.push(-f64::cos(2.0 * PI * i as f64 / 100.0));
     }
 
+    let data: Vec<(f64, f64)> = Vec::new();
+    let err: Vec<(f64, f64)> = Vec::new();
+    let log = runge4::<f64>(0.0, -1.0, PI / 32.0, 0.0, (data, err));
+    // draw_graph(0.0, 5.0 * PI, 0.0, 4.0, "x", "error", "blue", log.1.clone());
     let mut fg = Figure::new();
     {
         let axec = fg
             .axes2d()
             .set_x_axis(true, &[])
-            .set_x_range(Fix(2.9), Fix(18.1))
-            .set_y_range(Fix(0.0), Fix(7.0))
-            .set_x_label("p", &[])
-            .set_y_label("log_2E_r", &[])
+            .set_x_range(Fix(-2.5), Fix(2.5))
+            .set_y_range(Fix(-2.5), Fix(2.5))
+            .set_x_label("v_x", &[])
+            .set_y_label("v_y", &[])
             .lines(
-                line_x,
-                line_y,
-                &[Caption("least squares method"), Color("red")],
+                true_x,
+                true_y,
+                &[Caption("Analytical solution"), Color("red")],
             );
-        errs.iter().fold((), |_, e| {
+        log.0.iter().fold((), |_, e| {
             axec.points(&[e.0], &[e.1], &[Color("blue"), PointSymbol('O')]);
         });
         axec.points(
--- files/libraries/src/runge_kutta4.rs
+++ files/libraries/src/runge_kutta4.rs
@@ -1,3 +1,39 @@
-pub fn() {
-    //
+use num_traits::{Float, FromPrimitive};
+use std::f64::consts::PI;
+
+pub fn runge4<F>(
+    v_x: F,
+    v_y: F,
+    h: F,
+    t: F,
+    mut log: (Vec<(F, F)>, Vec<(F, F)>),
+) -> (Vec<(F, F)>, Vec<(F, F)>)
+where
+    F: Float + FromPrimitive,
+{
+    let hs = F::from_f64(1.0).unwrap()
+        + h
+        + h * h / F::from_f64(2.0).unwrap()
+        + h * h * h / F::from_f64(6.0).unwrap()
+        + h * h * h * h / F::from_f64(24.0).unwrap();
+    let v_x_n_1 = v_x + hs + v_y;
+    let v_y_n_1 = v_y - hs + v_x;
+    log.0.push((v_x_n_1, v_y_n_1));
+
+    let now = (t + h).to_f64().unwrap();
+
+    let err_norm = ((v_x_n_1 + F::from_f64(-f64::sin(now)).unwrap())
+        * (v_x_n_1 + F::from_f64(-f64::sin(now)).unwrap())
+        + (v_y_n_1 + F::from_f64(-f64::cos(now)).unwrap())
+            * (v_y_n_1 + F::from_f64(-f64::cos(now)).unwrap()))
+    .sqrt();
+    log.1.push((t, err_norm));
+
+    let now = F::from_f64(now).unwrap();
+
+    if now <= F::from_f64(5.0).unwrap() * F::from_f64(PI).unwrap() {
+        runge4(v_x_n_1, v_y_n_1, h, now, log)
+    } else {
+        log
+    }
 }
