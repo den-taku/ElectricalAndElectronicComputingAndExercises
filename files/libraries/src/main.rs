d47fcd7d2907718c49f0386a1b72599c290ed15b
DenTaku <dentaku1412@gmail.com>
2021-01-17 17:36:33 +0900

    Decide structure

--- files/libraries/src/main.rs
+++ files/libraries/src/main.rs
@@ -5,6 +5,7 @@ mod euler;
 mod heun;
 mod matrix;
 mod newton_raphson_method;
+mod pulus;
 mod runge_kutta4;
 
 use gnuplot::*;
@@ -17,7 +18,13 @@ use gnuplot::*;
 use draw::*;
 use euler::*;
 use heun::*;
+use pulus::*;
 use runge_kutta4::*;
 use std::f64::consts::PI;
 
-fn main() {}
+fn main() {
+    let init = vec![0f64; 100];
+    let data = vec![(-0.001, init.clone()), (0.0, init)];
+
+    let log = fixed_end_pulus(0.0, 5.0, data.clone());
+}
