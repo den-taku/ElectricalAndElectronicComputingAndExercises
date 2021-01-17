e7f0ca37a937725e4c6a1587783088660e4f6d06
DenTaku <dentaku1412@gmail.com>
2021-01-17 19:48:58 +0900

    Implement *_pulus2 functions

--- files/libraries/src/main.rs
+++ files/libraries/src/main.rs
@@ -29,20 +29,45 @@ fn main() {
     }
     let data = vec![(-0.001, init.clone()), (0.0, init)];
 
-    let log = fixed_end_pulus(0.0, 5.0, data.clone());
-
-    let u = log.iter().map(|e| {
-        e.1.iter().map(|v| {
-            v.1
-        }).collect::<Vec<f64>>()
-    }).collect::<Vec<Vec<f64>>>();
-    let u = u.concat();
+    // let log = fixed_end_pulus(0.0, 5.0, data.clone());
+    // let log = freed_end_pulus(0.0, 5.0, data.clone());
+    // let log = fixed_end_pulus2(0.0, 5.0, data.clone());
+    let log = freed_end_pulus2(0.0, 5.0, data.clone());
 
     draw_graph(0.0, 0.1, 0.0, 2.0, "x", "u", "blue", log[3000].1.clone());
 
+    // let u = log.iter().map(|e| {
+    //     e.1.iter().map(|v| {
+    //         v.1
+    //     }).collect::<Vec<f64>>()
+    // }).collect::<Vec<Vec<f64>>>();
+    // let u = u.concat();
+
+    // let x = {
+    //     let mut v = vec![];
+    //     for i in 0..5000 {
+    //         for j in 0..100 {
+    //             v.push(j as f64 * 1.0 / 100.0);
+    //         }
+    //     }
+    //     v
+    // };
+
+    // let y = {
+    //     let mut v = vec![];
+    //     for i in 0..5000 {
+    //         for j in 0..100 {
+    //             v.push(i as f64 * 0.001)
+    //         }
+    //     }
+    //     v
+    // };
+
     // let mut fg = Figure::new();
-    // fg.axes3d()
-    //     .surface()
-
-
+    // {
+    //     fg.axes3d()
+    //         .points(x, y, u, &[PointSymbol('o'), Color("blue"), PointSize(2.0)])
+    //         .set_title("Fix End Pulus", &[]);
+    // }
+    // fg.show();
 }
